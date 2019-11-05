5723e1e4078ec869b9da769c965b892b830c988d
FThelin <fredrikthelin82@gmail.com>
2019-11-05 21:30:20 +0100

    Changed colors and some animations in main page

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {  
+
+
+    // Navbar goes on top and stays on top when scrolling
+    $(window).scroll(() => {
+    if ($(window).scrollTop() >= $('.bottom-container').offset().top) {
+        $('.bottom-container').css({
+            position: 'fixed',
+            top: 0,
+            left: 0
+        }).animate({height: '7vh'},'slow');
+
+        $('.nav-container div').animate({
+            height: '2.5rem',
+            width: '2.5rem'
+        },'slow')
+        $('.nav-container i').animate({fontSize: '1.2rem'},'slow');
+        $('.nav-container').css('flex-direction', 'row');
+    }
+});
+
+    //Navbar hover effects
+    $('.nav-container').mouseover(function() {
+        $(this).css('backgroundColor', 'rgb(22, 22, 22)');
+        $('div', this).css( 'boxShadow', '0 0 5px rgb(0, 157, 230), 0 0 25px rgb(0, 157, 230)');
+    })
+
+    $('.nav-container').mouseout(function() {
+        $(this).css('backgroundColor', 'rgb(10, 10, 10)');
+        $('div', this).css( 'boxShadow', 'none');
+    })
+
+
+
+
+
+
+
+})
