0070a609dfc7f8d1f34d4038cb1640c1f0695893
Fredrik Thelin <fredrikthelin82@gmail.com>
2019-11-07 23:14:40 +0100

    fixed animations on about page

--- scripts/about.js
+++ scripts/about.js
@@ -1,16 +1,56 @@
 $(document).ready(() => {  
 
-    let top = -300;
+    const writeAboutText = text => {
+        $('.about-text p').animate({'opacity': 0}, 400, () => {
+            $('.about-text p').text(text).animate({'opacity': 1}, 400);    
+        });
+    }
 
-    $('.running-man').mouseover(() => {
-        
-        if (top >= -600) {
-            $('.running-man img').animate({
-                top: top
-            },'slow')
-            top -= 300;
+    const changeAboutText = () => {
+        switch (position){
+            case 0:
+                writeAboutText('Born and raised in Vadstena, Sweden!');
+                break;
+            case -500:
+                writeAboutText('Ekonomisk linje');
+                break;
+            case -1000:
+                writeAboutText('Musikpedagog');
+                break;
+            case -1500:
+                writeAboutText('Hamnen');
+                break;
+            case -2000:
+                writeAboutText('Medieinstitutet');
+                break;
+            default:
+                writeAboutText('def');
+                break;
         }
-        
+    }
+
+    let position = 0;
+
+    $('.slide-up').click(() => {        
+        if (position <= -500) {            
+            position += 500;  
+            console.log(position)                  
+            $('.running-man img').animate({
+                top: position
+            },'slow');
+            changeAboutText();
+        }
+    })
+
+    $('.slide-down').click(() => {        
+        if (position > -2000) {            
+            position -= 500;  
+            console.log(position)                  
+            $('.running-man img').animate({
+                top: position
+            },'slow');
+            changeAboutText();
+        }    
     })
 
 
