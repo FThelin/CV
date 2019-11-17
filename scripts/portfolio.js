50a128ee0710f6a3faa4b0c950a30170e77b3d21
FThelin <fredrikthelin82@gmail.com>
2019-11-17 23:34:45 +0100

    changed image animations

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -7,19 +7,27 @@ $(document).ready(() => {
         
         if (degree === 90){
             $('.outer-circle').css('transform', `rotate(${degree}deg)`);
-            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`);  
+            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`);
+            $('.tele2-image').css('opacity', '1');
+            $('.inner-circle img').css('transform', `rotate(-${degree}deg)`);   
             degree = 180;
         } else if (degree === 180){
             $('.outer-circle').css('transform', `rotate(${degree}deg)`);
-            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`);   
+            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`);
+            $('.inner-circle img').attr('src', `../img/tele2.png`);
+            $('.inner-circle img').css('transform', `rotate(-${degree}deg)`);     
             degree = 270;
         } else if (degree === 270){
             $('.outer-circle').css('transform', `rotate(${degree}deg)`); 
-            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`);  
+            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`); 
+            $('.inner-circle img').attr('src', `../img/tele2.png`);
+            $('.inner-circle img').css('transform', `rotate(-${degree}deg)`);   
             degree = 0;
         } else if (degree === 0){
             $('.outer-circle').css('transform', `rotate(${degree}deg)`);
-            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`);   
+            $('.outer-circle p').css('transform', `rotate(-${degree}deg)`); 
+            $('.inner-circle img').attr('src', `../img/tele2.png`);
+            $('.inner-circle img').css('transform', `rotate(-${degree}deg)`);    
             degree = 90;
         }
         
